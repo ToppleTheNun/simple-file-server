ffe1389f1a240694f297cc940776b1259be4443a
Richard Harrah <richard.harrah2@anthem.com>
2015-12-07 14:11:09 -0500

    adding examples

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var express = require('express');
+var serveIndex = require('serve-index');
 var app = express();
 
 var oneDay = 86400000;
 
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/files', { maxAge: oneDay }));
+app.use(serveIndex('public', { icons: true, view: 'details' }));
+app.use(express.static(__dirname + '/public', { maxAge: oneDay }));
 
 app.listen(app.get('port'), function() {
     console.log("Node app is running at localhost:" + app.get('port'))
