8c292cd7ae349b3478f20929e12dcbf658ae6b6c
Richard Harrah <Nunnery@users.noreply.github.com>
2018-01-08 15:40:30 -0500

    Update app.js

--- app.js
+++ app.js
@@ -18,6 +18,6 @@ var httpsOptions = {
     passphrase: appConfig.passphrase
 };
 
-https.createServer(options, app).listen(app.get('port'), function() {
+https.createServer(httpsOptions, app).listen(app.get('port'), function() {
     console.log("Node app is running at localhost:" + app.get('port'));
 });
