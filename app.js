c953f9cc6e1b8a56214005c1433d99182dd50cd9
Richard Harrah <Nunnery@users.noreply.github.com>
2018-01-08 15:39:07 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+var fs = require('fs');
+var https = require('https');
+
 var express = require('express');
 var serveIndex = require('serve-index');
 var app = express();
+
+var appConfig = require('./app.json');
 
 var oneDay = 86400000;
 
@@ -8,6 +13,11 @@ app.set('port', (process.env.PORT || 5000));
 app.use(serveIndex('public', { icons: true, view: 'details' }));
 app.use(express.static(__dirname + '/public', { maxAge: oneDay }));
 
-app.listen(app.get('port'), function() {
-    console.log("Node app is running at localhost:" + app.get('port'))
+var httpsOptions = {
+    pfx: fs.readFileSync(appConfig.pfx),
+    passphrase: appConfig.passphrase
+};
+
+https.createServer(options, app).listen(app.get('port'), function() {
+    console.log("Node app is running at localhost:" + app.get('port'));
 });
